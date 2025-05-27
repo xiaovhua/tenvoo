2a56841fb2804e922eb80c0aa3d41e3d29bd8bd9
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2025-05-27 15:18:06 +0900

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,36 @@
+import glob
+from monai import transforms
+from torch.utils.data import Dataset
+
+
+def default_transform(spatial_size=(160, 224, 160), scale_ratio=1., minv=0, maxv=1):
+    return transforms.Compose([
+        transforms.EnsureChannelFirst(),
+        transforms.ResizeWithPadOrCrop(spatial_size=spatial_size, mode='minimum'),
+        transforms.Orientation(axcodes='RAS'),
+        transforms.Spacing(pixdim=scale_ratio),
+        transforms.ScaleIntensity(minv=minv, maxv=maxv)
+    ])
+
+
+class DDPMDataset(Dataset):
+    def __init__(self, root, transform=None):
+        super().__init__()
+        if isinstance(root, list):
+            self.input_list = []
+            for r in root:
+                self.input_list.extend(glob.glob(r))
+        else:
+            self.input_list = glob.glob(root)
+        self.transform = transform
+        self.loader = transforms.LoadImage(image_only=True)
+
+    def __len__(self):
+        return len(self.input_list)
+
+    def __getitem__(self, item):
+        x = self.loader(self.input_list[item])
+        if self.transform is not None:
+            x = self.transform(x)
+        return x
+
