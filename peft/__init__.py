cd9fef54a35a1c0c41b6190e2df620841fdb253f
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2025-06-02 12:02:29 +0900

    Add files via upload

---
+++ peft/__init__.py
@@ -0,0 +1,4 @@
+from .locon import LoConConfig, LoConModel
+from .lokr import LokrConfig, LokrModel
+from .loha import LohaConfig, LohaModel
+from .tenvoo import TenVOOConfig, TenVOOModel, TENVOO_LIST
