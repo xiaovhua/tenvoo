c5c4f16a57587235609ae996fca2d39b223f1422
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2025-06-02 13:21:43 +0900

    Update run_ft_ddpm.sh

--- scripts/run_ft_ddpm.sh
+++ scripts/run_ft_ddpm.sh
@@ -17,7 +17,7 @@ ddpm_steps=1000
 unet_ckpt=./path/to/pretrained/ddpm_unet
 
 ###################################### peft settings ######################################
-# # select from (ff, lora, locon, lokr, loha, tenvoo-l, tenvoo-q)
+# # select from (ff, lora or locon, lokr, loha, tenvoo-l, tenvoo-q)
 ft_mode=tenvoo-l
 rank=4
 # # select from (conv1, conv2, skip, ds, us, to_q, to_k, to_v, attn_proj, time_proj, time_emb) for monai diffusion model
